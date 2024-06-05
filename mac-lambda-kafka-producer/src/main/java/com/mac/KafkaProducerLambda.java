a82f53e9b9972f70245fd1692b7decd584992cd5
Vijay <a1559601@bestbuy.com>
2024-06-05 15:05:24 -0500

    producer code

---
+++ mac-lambda-kafka-producer/src/main/java/com/mac/KafkaProducerLambda.java
@@ -0,0 +1,97 @@
+package com.mac;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Author: Vijay Krishna
+ * Kafka Producer as Lambda Function Build to support s3 event and lambda test function events
+ */
+public class KafkaProducerLambda implements RequestHandler<Object, String> {
+    private static final String TOPIC_NAME = "mac_pos";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        try {
+            context.getLogger().log("Input Event:" +     input);
+            context.getLogger().log("Input Event:" + input.getClass());
+            if (input instanceof Map && ((Map<?, ?>) input).containsKey("Records")) {
+                context.getLogger().log("S3 event detected.");
+//                ObjectMapper objectMapper = new ObjectMapper();
+//
+//                String jsonString = objectMapper.writeValueAsString(input);
+//                context.getLogger().log("jsonString:"+jsonString);
+//
+//                // Register custom deserializer
+//                SimpleModule module = new SimpleModule();
+//                module.addDeserializer(S3Event.class, new S3EventDeserializer());
+//                objectMapper.registerModule(module);
+//
+//                // Convert JSON string to S3Event
+//                S3Event s3Event = objectMapper.readValue(jsonString, S3Event.class);
+
+                // Convert the input map to a JSON string
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonString = objectMapper.writeValueAsString(input);
+
+                // Deserialize the JSON string to S3Event
+                S3Event s3Event = objectMapper.readValue(jsonString, S3Event.class);
+
+                // Process the S3Event
+                context.getLogger().log("s3Event: " + s3Event);
+//                LinkedHashMap<String, Object> inputMap = (LinkedHashMap<String, Object>) input;
+//                Gson gson = new GsonBuilder().create();
+//                // Convert inputMap to JSON string
+//                String jsonString = gson.toJson(inputMap);
+//                context.getLogger().log("jsonString:"+jsonString);
+//
+//                // Deserialize JSON string to S3Event object
+//                S3Event s3Event = gson.fromJson(jsonString, S3Event.class);
+//                context.getLogger().log("s3Event Generated:"+s3Event);
+//                // Log the S3Event object for debugging
+//                context.getLogger().log("S3Event to string: " + s3Event.toString());
+
+                return handleS3Event(s3Event, context);
+            } else {
+                context.getLogger().log("Non-S3 event detected.");
+                return handleNormalEvent(input, context);
+            }
+        } catch (Exception ex) {
+            context.getLogger().log("Exception in handleRequest :" + ex);
+        }
+        return null;
+    }
+
+    private String handleS3Event(S3Event s3Event, Context context) {
+        S3Processor s3Processor = new S3Processor();
+        return s3Processor.processS3Event(s3Event, context);
+    }
+
+
+    private String handleNormalEvent(Object input, Context context) {
+        OtherProcessor otherProcessor = new OtherProcessor();
+        return otherProcessor.handleNormalEvent(parseMessage(input), context);
+    }
+
+    private String parseMessage(Object input) {
+        if (input instanceof Map) {
+            try {
+                return OBJECT_MAPPER.writeValueAsString(input);
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ mac-lambda-kafka-producer/src/main/java/com/mac/S3Processor.java
@@ -0,0 +1,128 @@
+package com.mac;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.Future;
+
+/**
+ * Author: Vijay Krishna
+ * Handle S3 bucket events
+ */
+public class S3Processor {
+
+    private static final String TOPIC_NAME = "mac_pos";
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static final String KEY = "S3-Event";
+
+
+    public String processS3Event(S3Event s3Event, Context context) {
+        context.getLogger().log("S3Event : " + s3Event);
+        context.getLogger().log("Context : " + context);
+        Properties props = null;
+        try {
+            props = readConfig("client.properties");
+        } catch (IOException e) {
+            context.getLogger().log("Failed to load configuration: " + e.getMessage());
+        }
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        for (S3EventNotification.S3EventNotificationRecord record : s3Event.getRecords()) {
+            String bucketName = record.getS3().getBucket().getName();
+            String objectKey = record.getS3().getObject().getKey();
+            context.getLogger().log("BucketName :{} and ObjectKey: {}" + bucketName + objectKey);
+            // Read file content from S3
+            String fileContent;
+            try {
+                fileContent = readS3Object(bucketName, objectKey);
+            } catch (Exception e) {
+                context.getLogger().log("Error reading S3 object: " + e.getMessage());
+                break;
+            }
+//            String message = parseMessage(fileContent);
+            context.getLogger().log("FileContent:" + fileContent);
+            ProducerRecord<String, String> kafkaRecord = new ProducerRecord<>(TOPIC_NAME, KEY, fileContent);
+
+            try {
+                Future<RecordMetadata> future = producer.send(kafkaRecord, (metadata, exception) -> {
+                    if (exception == null) {
+                        context.getLogger().log("Message sent successfully: " + metadata.toString());
+                    } else {
+                        context.getLogger().log("Error sending message: " + exception.getMessage());
+                    }
+                });
+                RecordMetadata metadata = future.get();
+                context.getLogger().log("Message sent to topic: " + metadata.topic() + " partition: " + metadata.partition() + " offset: " + metadata.offset());
+
+            } catch (Exception e) {
+                context.getLogger().log("Error producing messages: " + e.getMessage());
+                break;
+            }
+        }
+
+        return "Processed S3 event and sent to Kafka topic: " + TOPIC_NAME;
+    }
+
+    private String readS3Object(String bucketName, String objectKey) {
+        InputStream inputStream = null;
+        try {
+            S3Client s3Client = S3Client.builder().region(Region.US_EAST_1).build(); // Adjust region as needed
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(objectKey)
+                    .build();
+
+            inputStream = s3Client.getObject(getObjectRequest);
+
+        } catch (Exception e) {
+        }
+        return new Scanner(inputStream).useDelimiter("\\A").next();
+    }
+
+    public Properties readConfig(String configFile) throws IOException {
+        Properties props = new Properties();
+
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFile)) {
+            if (input == null) {
+                throw new IOException(configFile + " not found.");
+            }
+            props.load(input);
+        }
+
+        // Add Kafka producer specific properties
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return props;
+    }
+
+    private String parseMessage(Object input) {
+        if (input instanceof Map) {
+            try {
+                Map<String, Object> inputMap = (Map<String, Object>) input;
+                inputMap.put("key", "S3");
+                return OBJECT_MAPPER.writeValueAsString(input);
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
