c9c5a1ab39ebeb3bc7524c5738f066af93dc2ad9
a1567309 <a1567309@bestbuy.com>
2024-06-27 17:27:03 +0530

    loggers added

--- mac-lambda-kafka-producer/src/main/java/com/mac/XMLReader.java
+++ mac-lambda-kafka-producer/src/main/java/com/mac/XMLReader.java
@@ -20,6 +20,8 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class XMLReader {
 
